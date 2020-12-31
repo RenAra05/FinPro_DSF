710c3f3fe879d3afe433b20da2ea98f0ff98ccea
RenAra05 <ren.araujo.05@gmail.com>
2020-12-31 22:47:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 
 st.set_option('deprecation.showfileUploaderEncoding',False)
-@st.cache(allow_output_mutation=true)
+@st.cache(allow_output_mutation=True)
 def load_model():
   model=tf.keras.models.load_model('/content/flower_identification.hdf5')
   return model
@@ -22,6 +22,7 @@ def import_and_predict(image_data, model):
   image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
   img = np.asarray(image)
   img_reshape = img[np.newaxis,...]
+  prediction = model.predict(img_reshape)
 
   return prediction
 if file is None:
